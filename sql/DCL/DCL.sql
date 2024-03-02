fccb8731e3ed36d9c25d3bf401c598019446530a
choisimo <csimon2k0209@gmail.com>
2024-03-02 15:43:19 +0900

    join sample

--- sql/DCL/DCL.sql
+++ sql/DCL/DCL.sql
@@ -1,1 +1,4 @@
 -- GRANT, REVOKE
+
+# GRANT
+GRANT ALL PRIVILEGES ON ${database}.* to ${user} IDENTIFIED BY ${password};
--- sql/DDL/DDL.sql
+++ sql/DDL/DDL.sql
@@ -4,3 +4,4 @@ create table hello(
     id bigint AUTO_INCREMENT PRIMARY key,
     username varchar(50)
 );
+
